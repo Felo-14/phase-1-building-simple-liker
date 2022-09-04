928ea7046004b14975a416ea93b9bd0b61faebad
felix mwendwa <felix.mwendwa@student.moringaschool.com>
2022-09-04 16:29:09 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,8 +3,22 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
+const articleHearts = document.querySelectorAll('.like-glyph');
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall('bogusUrl')
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = 'activated-heart';
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = '';
+      }
+    })
+}
+const modal = document.getElementById('modal');
+modal.classList.add('hidden');
 
 
 //------------------------------------------------------------------------------
